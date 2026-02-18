43c0f9cfeea6ccef42eadeeaabcb16f30f547824
hiruvindajayashanka2001 <hiruvindajayashanka2001@gmail.com>
2026-02-18 18:57:04 +0530

    simple http request using https module

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ console.log("Node is working!");
 
 const fs = require("fs");
 const http = require('http');
+const https = require('https');
 
 
 fs.readFile("file.txt", "utf8", (err, data) => {
@@ -23,5 +24,20 @@ http.createServer(function (req, res) {
   res.write('Hello World!');
   res.end();
 }).listen(8083);
+
+https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
+  let data = '';
+
+  resp.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  resp.on('end', () => {
+    console.log(JSON.parse(data));
+  });
+
+}).on('error', (err) => {
+  console.log("Error: " + err.message);
+});
 
 
