7be6cf04a9c1f719f10de704a3c63562a9c2089e
hiruvindajayashanka2001 <hiruvindajayashanka2001@gmail.com>
2026-02-18 18:34:07 +0530

    create example to read file using fs module

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 console.log("Node is working!");
+
+
+const fs = require("fs");
+
+fs.readFile("file.txt", "utf8", (err, data) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(data);
+});
